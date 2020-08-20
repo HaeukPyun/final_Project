5c0a0b6e349774893e24f3beafb945c506769850
taegyukim1 <68097393+taegyukim1@users.noreply.github.com>
2020-08-20 16:36:13 +0900

    page7 update
    페이지7 내용 업로드

--- TeamProject/src/main/java/com/myteamproject/myapp/HomeController.java
+++ TeamProject/src/main/java/com/myteamproject/myapp/HomeController.java
@@ -36,4 +36,18 @@ public class HomeController {
 		return "home";
 	}
 	
+	@RequestMapping(value = "/page7", method = RequestMethod.GET)
+	public String page(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "page7";
+	}
+	
 }
